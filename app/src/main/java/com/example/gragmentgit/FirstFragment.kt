41213033f8042b208d351d093f52848f9f56e26e
Islamova12345 <gislamova@std.beu.edu.az>
2024-08-10 01:54:37 +0400

    SafeArgadd

--- app/src/main/java/com/example/gragmentgit/FirstFragment.kt
+++ app/src/main/java/com/example/gragmentgit/FirstFragment.kt
@@ -1,59 +1,39 @@
 package com.example.gragmentgit
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FirstFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var editText1: EditText
+    private lateinit var editText2: EditText
+    private lateinit var addButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FirstFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FirstFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        editText1 = view.findViewById(R.id.editText1)
+        editText2 = view.findViewById(R.id.editText2)
+        addButton = view.findViewById(R.id.addButton)
+
+        addButton.setOnClickListener {
+            val number1 = editText1.text.toString().toIntOrNull() ?: 0
+            val number2 = editText2.text.toString().toIntOrNull() ?: 0
+            val sum = number1 + number2
+
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(sum)
+            findNavController().navigate(action)
+        }
+
+        return view
     }
 }
