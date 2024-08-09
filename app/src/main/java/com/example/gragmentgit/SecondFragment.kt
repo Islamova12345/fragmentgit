4ae3fd4040dff1a576d4fd7008811f2940997652
Islamova12345 <gislamova@std.beu.edu.az>
2024-08-10 02:27:00 +0400

    BackButton

--- app/src/main/java/com/example/gragmentgit/SecondFragment.kt
+++ app/src/main/java/com/example/gragmentgit/SecondFragment.kt
@@ -3,13 +3,16 @@ package com.example.gragmentgit
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 class SecondFragment : Fragment() {
-
+    private lateinit var back:Button
     private val args: SecondFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -17,10 +20,13 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_second, container, false)
-
+        back=view.findViewById(R.id.back)
         val resultTextView: TextView = view.findViewById(R.id.resultTextView)
         resultTextView.text = args.sumResult.toString()
-
+        back.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            findNavController().navigate(action)
+        }
         return view
     }
 }
