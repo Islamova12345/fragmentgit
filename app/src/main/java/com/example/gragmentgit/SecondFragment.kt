8c9631fff31806f5bcbfd4b4dffed9cd3999aaf4
Islamova12345 <gislamova@std.beu.edu.az>
2024-08-10 16:15:29 +0400

    secondfragmentdedeyisiklik

--- app/src/main/java/com/example/gragmentgit/SecondFragment.kt
+++ app/src/main/java/com/example/gragmentgit/SecondFragment.kt
@@ -1,32 +1,25 @@
 package com.example.gragmentgit
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.gragmentgit.databinding.FragmentSecondBinding
 
-class SecondFragment : Fragment() {
-    private lateinit var back:Button
+class SecondFragment : Fragment(R.layout.fragment_second) {
     private val args: SecondFragmentArgs by navArgs()
+    private lateinit var binding: FragmentSecondBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_second, container, false)
-        back=view.findViewById(R.id.back)
-        val resultTextView: TextView = view.findViewById(R.id.resultTextView)
-        resultTextView.text = args.sumResult.toString()
-        back.setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSecondBinding.bind(view)
+
+        binding.resultTextView.text = args.sumResult.toString()
+
+        binding.back.setOnClickListener {
             val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
             findNavController().navigate(action)
         }
-        return view
     }
 }
